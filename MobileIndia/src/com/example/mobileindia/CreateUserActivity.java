53867fa21e24ac63c311411472ecb401120bc9f3
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-03-05 15:41:56 -0500

    Almost done with create user form

--- MobileIndia/src/com/example/mobileindia/CreateUserActivity.java
+++ MobileIndia/src/com/example/mobileindia/CreateUserActivity.java
@@ -1,13 +1,23 @@
 package com.example.mobileindia;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.telephony.PhoneNumberUtils;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 
 public class CreateUserActivity extends Activity {
-
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+    private EditText mPasswordConfirmView;
+    private EditText mPhoneView;
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,6 +25,7 @@ public class CreateUserActivity extends Activity {
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,6 +33,56 @@ public class CreateUserActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_create_user, menu);
 		return true;
 	}
+	 public void attemptCreateUser(View view){
+		 mUsernameView = (EditText) findViewById(R.id.username);
+		 mPasswordView = (EditText) findViewById(R.id.password_create);
+		 mPasswordConfirmView = (EditText) findViewById(R.id.password_confirm_create);
+		 mPhoneView = (EditText) findViewById(R.id.phone);
+		 boolean cancel = false;
+		 View focusView = null;
+		 View focusView2 = null;
+		 if (TextUtils.isEmpty(mUsernameView.getText().toString())){
+				mUsernameView.setError(getString(R.string.error_field_required));
+				focusView = mUsernameView;
+				cancel = true;
+		 }
+		 if (TextUtils.isEmpty(mPasswordView.getText().toString())){
+				mPasswordView.setError(getString(R.string.error_field_required));
+				focusView = mPasswordView;
+				cancel = true;
+		 }
+		 if (TextUtils.isEmpty(mPasswordConfirmView.getText().toString())){
+				mPasswordConfirmView.setError(getString(R.string.error_field_required));
+				focusView = mPasswordConfirmView;
+				cancel = true;
+		 }else if (!TextUtils.equals(mPasswordView.getText().toString(), mPasswordConfirmView.getText().toString())){
+				mPasswordConfirmView.setError("password and password confirm must match");
+				focusView = mPasswordConfirmView;
+				cancel = true;
+		 }
+		 if(TextUtils.isEmpty(mPhoneView.getText().toString())){
+				mPhoneView.setError("A phone number is required");
+				focusView = mPhoneView;
+				cancel = true;
+		 }else if (!PhoneNumberUtils.isGlobalPhoneNumber(mPhoneView.getText().toString())){
+				mPhoneView.setError("phone number is invalid");
+				focusView = mPhoneView;
+				cancel = true;
+		 }
+
+	     if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		 } else {
+				// Show a progress spinner, and kick off a background task to
+				// perform the user login attempt.
+				//mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			//	showProgress(true);
+				//mAuthTask = new UserLoginTask();
+			//	mAuthTask.execute((Void) null);
+			}
+	 }
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
