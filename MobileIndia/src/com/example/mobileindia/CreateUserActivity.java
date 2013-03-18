6c10b300d17132159b5d2f2fd3f6f09e6bb59bb4
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-03-18 15:51:24 -0400

    Need brians files

--- MobileIndia/src/com/example/mobileindia/CreateUserActivity.java
+++ MobileIndia/src/com/example/mobileindia/CreateUserActivity.java
@@ -1,22 +1,43 @@
 package com.example.mobileindia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.example.mobileindia.LoginActivity.UserLoginTask;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.NavUtils;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreateUserActivity extends Activity {
     private EditText mUsernameView;
     private EditText mPasswordView;
     private EditText mPasswordConfirmView;
     private EditText mPhoneView;
+    private EditText mEmailView;
+	private View mCreateUserFormView;
+	private View mCreateUserStatusView;
+	private TextView mCreateUserStatusMessageView;
+	private UserCreateTask mAuthTask = null;
+	private static boolean loginSuccess;
+	public static int loginErrorCode;
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +45,30 @@ public class CreateUserActivity extends Activity {
 		setContentView(R.layout.activity_create_user);
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		mCreateUserFormView = findViewById(R.id.create_user_form);
+		mCreateUserStatusView = findViewById(R.id.create_user_status);
+		mCreateUserStatusMessageView = (TextView) findViewById(R.id.create_user_status_message);
 	}
 	
+	/**
+	 * method is used for checking valid email id format.
+	 * 
+	 * @param email
+	 * @return boolean true for valid false for invalid
+	 */
+	public static boolean isEmailValid(String email) {
+	    boolean isValid = false;
 
+	    String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+	    CharSequence inputStr = email;
+
+	    Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher(inputStr);
+	    if (matcher.matches()) {
+	        isValid = true;
+	    }
+	    return isValid;
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -38,25 +80,16 @@ public class CreateUserActivity extends Activity {
 		 mPasswordView = (EditText) findViewById(R.id.password_create);
 		 mPasswordConfirmView = (EditText) findViewById(R.id.password_confirm_create);
 		 mPhoneView = (EditText) findViewById(R.id.phone);
+		 mEmailView = (EditText) findViewById(R.id.email);
 		 boolean cancel = false;
 		 View focusView = null;
-		 if (TextUtils.isEmpty(mUsernameView.getText().toString())){
-				mUsernameView.setError(getString(R.string.error_field_required));
-				focusView = mUsernameView;
+		 if (TextUtils.isEmpty(mEmailView.getText().toString())) {
+				mEmailView.setError("An email is required");
+				focusView = mEmailView;
 				cancel = true;
-		 }
-		 if (TextUtils.isEmpty(mPasswordView.getText().toString())){
-				mPasswordView.setError(getString(R.string.error_field_required));
-				focusView = mPasswordView;
-				cancel = true;
-		 }
-		 if (TextUtils.isEmpty(mPasswordConfirmView.getText().toString())){
-				mPasswordConfirmView.setError(getString(R.string.error_field_required));
-				focusView = mPasswordConfirmView;
-				cancel = true;
-		 }else if (!TextUtils.equals(mPasswordView.getText().toString(), mPasswordConfirmView.getText().toString())){
-				mPasswordConfirmView.setError("password and password confirm must match");
-				focusView = mPasswordConfirmView;
+		 }else if(!isEmailValid(mEmailView.getText().toString())){
+			 	mEmailView.setError("A valid email is required");
+				focusView = mEmailView;
 				cancel = true;
 		 }
 		 if(TextUtils.isEmpty(mPhoneView.getText().toString())){
@@ -68,7 +101,25 @@ public class CreateUserActivity extends Activity {
 				focusView = mPhoneView;
 				cancel = true;
 		 }
-
+		 if (TextUtils.isEmpty(mPasswordConfirmView.getText().toString())){
+				mPasswordConfirmView.setError(getString(R.string.error_field_required));
+				focusView = mPasswordConfirmView;
+				cancel = true;
+		 }else if (!TextUtils.equals(mPasswordView.getText().toString(), mPasswordConfirmView.getText().toString())){
+				mPasswordConfirmView.setError("password and password confirm must match");
+				focusView = mPasswordConfirmView;
+				cancel = true;
+		 }
+		 if (TextUtils.isEmpty(mPasswordView.getText().toString())){
+				mPasswordView.setError(getString(R.string.error_field_required));
+				focusView = mPasswordView;
+				cancel = true;
+		 }
+		 if (TextUtils.isEmpty(mUsernameView.getText().toString())){
+				mUsernameView.setError(getString(R.string.error_field_required));
+				focusView = mUsernameView;
+				cancel = true;
+		 }
 	     if (cancel) {
 			// There was an error; don't attempt login and focus the first
 			// form field with an error.
@@ -76,13 +127,103 @@ public class CreateUserActivity extends Activity {
 		 } else {
 				// Show a progress spinner, and kick off a background task to
 				// perform the user login attempt.
-				//mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-			//	showProgress(true);
-				//mAuthTask = new UserLoginTask();
-			//	mAuthTask.execute((Void) null);
+				mCreateUserStatusMessageView.setText(R.string.login_progress_signing_in);
+			    showProgress(true);
+			    mAuthTask = new UserCreateTask();
+			    mAuthTask.execute((Void) null);
 			}
 	 }
 
+	/**
+		 * Shows the progress UI and hides the login form.
+		 */
+		@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+		private void showProgress(final boolean show) {
+			// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+			// for very easy animations. If available, use these APIs to fade-in
+			// the progress spinner.
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+				int shortAnimTime = getResources().getInteger(
+						android.R.integer.config_shortAnimTime);
+
+				mCreateUserStatusView.setVisibility(View.VISIBLE);
+				mCreateUserStatusView.animate().setDuration(shortAnimTime)
+						.alpha(show ? 1 : 0)
+						.setListener(new AnimatorListenerAdapter() {
+							@Override
+							public void onAnimationEnd(Animator animation) {
+								mCreateUserStatusView.setVisibility(show ? View.VISIBLE
+										: View.GONE);
+							}
+						});
+
+				mCreateUserFormView.setVisibility(View.VISIBLE);
+				mCreateUserFormView.animate().setDuration(shortAnimTime)
+						.alpha(show ? 0 : 1)
+						.setListener(new AnimatorListenerAdapter() {
+							@Override
+							public void onAnimationEnd(Animator animation) {
+								mCreateUserFormView.setVisibility(show ? View.GONE
+										: View.VISIBLE);
+							}
+						});
+			} else {
+				// The ViewPropertyAnimator APIs are not available, so simply show
+				// and hide the relevant UI components.
+				mCreateUserStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+				mCreateUserFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+			}
+		}
+		/**
+		 * Represents an asynchronous login/registration task used to authenticate
+		 * the user.
+		 */
+		public class UserCreateTask extends AsyncTask<Void, Void, Boolean> {
+			@Override
+			protected Boolean doInBackground(Void... params) {
+				try {
+					ParseUser.logIn("test_user", "");
+					CreateUserActivity.loginSuccess = true;
+				} catch (ParseException e) {
+					Log.d("PARSE","Signup FAILED");
+					Log.d("PARSE", "ERROR CODE: " + String.valueOf(e.getCode()));
+					CreateUserActivity.loginErrorCode = e.getCode();
+				}
+				return CreateUserActivity.loginSuccess;
+			}
+
+			@Override
+			protected void onPostExecute(final Boolean success) {
+				mAuthTask = null;
+				showProgress(false);
+				if (success) {
+					finish();
+					Context context = getApplicationContext();
+					int duration = Toast.LENGTH_LONG;
+					Toast toast = Toast.makeText(context, "Successfully logged in!", duration);
+					toast.show();
+				} else {
+					switch (CreateUserActivity.loginErrorCode) {
+					case ParseException.OBJECT_NOT_FOUND:
+						mUsernameView.setError("No account with this username and password found");
+						mUsernameView.requestFocus();
+						break;
+					case ParseException.VALIDATION_ERROR:
+						mPasswordView.setError("Invalid password.");
+						mPasswordView.requestFocus();
+					default:
+						break;
+					}
+				}
+			}
+
+			@Override
+			protected void onCancelled() {
+				mAuthTask = null;
+				showProgress(false);
+			}
+		}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
