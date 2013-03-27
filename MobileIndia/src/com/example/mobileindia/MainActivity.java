cc65d7f3e277d337d2ae1f4281a7ed2f34d91f90
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-03-27 18:14:20 -0400

    Began logout code and cont logged in as label

--- MobileIndia/src/com/example/mobileindia/MainActivity.java
+++ MobileIndia/src/com/example/mobileindia/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.mobileindia;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseUser;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     public static boolean anonSuccess;
@@ -23,7 +23,17 @@ public class MainActivity extends Activity {
 	    Parse.initialize(this, "dlhBQJUyZsOPxkFdp8Uf7MWXY7IpRWXXZipSyO8f", "sa5JGVnNQXEoWawJPxw5TKk1xLmFirXcGMr5P5JK");
 		setContentView(R.layout.activity_main);
 	}
-
+	protected void onPostCreate (Bundle savedInstanceState){
+		super.onPostCreate(savedInstanceState);
+		TextView t = (TextView) findViewById(R.id.mainActivityUserLabel);
+		if (ParseUser.getCurrentUser() == null) {
+			t.setText("Not logged in");
+		}else if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())){ 
+			t.setText("Logged in anonymously");
+	    }else {
+			t.setText("Logged in as: " + ParseUser.getCurrentUser().getUsername());
+		}
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
